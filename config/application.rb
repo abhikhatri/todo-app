ba39683c2f0b5ea4061d30359fa394f998c03717
Prashant Kumar Mishra <pmmishra78@gmail.com>
2016-02-20 15:19:02 +0530

    Added devise gem and welcome file

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module TodoTimer
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -21,6 +23,8 @@ module TodoTimer
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
